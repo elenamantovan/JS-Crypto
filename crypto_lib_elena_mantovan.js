7f20c3d4a4bcb7a01f8b52ea65ee43713fadcf64
elenamantovan <elena.mantovan84@gmail.com>
2025-02-28 14:35:56 +0100

    Partie 1 ok

---
+++ crypto_lib_elena_mantovan.js
@@ -0,0 +1,15 @@
+//Demande à l'utilisateur de saisir un mot, calcule sa longueur et demande une confirmation.
+//Retourne vrai si l'utilisateur confirme la longueur correcte, sinon faux.
+
+function what_are_words_worth() {
+    // Instructions pour l'utilisateur
+    let valeur_entree = prompt("Veuillez écrire quelque chose :");
+    //Calcule la longueur
+    let length = valeur_entree.length;
+    // Demande la confirmation
+    let confirmation = confirm('Votre entrée "' + valeur_entree + '" comporte ' + length + ' caractère(s).');
+    // Renvoie la valeur booléenne basée sur la confirmation
+    return confirmation;
+
+
+}
