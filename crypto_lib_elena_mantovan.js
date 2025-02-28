559d304ef01973c136dccd83ff560cf102e535df
elenamantovan <elena.mantovan84@gmail.com>
2025-02-28 15:02:26 +0100

    partie 2 ok

--- crypto_lib_elena_mantovan.js
+++ crypto_lib_elena_mantovan.js
@@ -10,6 +10,30 @@ function what_are_words_worth() {
     let confirmation = confirm('Votre entrée "' + valeur_entree + '" comporte ' + length + ' caractère(s).');
     // Renvoie la valeur booléenne basée sur la confirmationr
     return confirmation;
-
-
 }
+
+
+// Cette fonction prend en paramètre une chaîne de caractères (string_of_words)
+// et retourne le nombre de mots dans cette chaîne.
+// On suppose que les mots sont séparés uniquement par des espaces.
+// La fonction parcourt la chaîne caractère par caractère et compte les espaces
+// pour déterminer le nombre de mots.
+
+function count_words(string_of_words) {
+ // Initialisation du compteur de mots à 1 car le nombre de mots est égal au nombre d'espaces + 1
+let wordCount = 1;
+
+    // Parcours de la chaîne de caractères
+        for (let i = 0; i < string_of_words.length; i++) {
+
+            // Si le caractère courant est un espace, on incrémente le compteur de mots
+            if (string_of_words.charAt(i) === ' ') {
+                wordCount++;
+            }
+        }
+// Retourne le nombre de mots comptés
+return wordCount;
+}
+
+
+    
