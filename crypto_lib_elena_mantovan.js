0049c6fa01b8e645544641d640d0beda32247959
elenamantovan <elena.mantovan84@gmail.com>
2025-02-28 16:06:15 +0100

    Partie 3 ok

--- crypto_lib_elena_mantovan.js
+++ crypto_lib_elena_mantovan.js
@@ -1,4 +1,4 @@
-//Demande à l'utilisateur de saisir un mot, calcule sa longueur et demande une confirmation.
+//Partie 1: Demande à l'utilisateur de saisir un mot, calcule sa longueur et demande une confirmation.
 //Retourne vrai si l'utilisateur confirme la longueur correcte, sinon faux.
 
 function what_are_words_worth() {
@@ -13,13 +13,14 @@ function what_are_words_worth() {
 }
 
 
-// Cette fonction prend en paramètre une chaîne de caractères (string_of_words)
+// Partie 2: Cette fonction prend en paramètre une chaîne de caractères (string_of_words)
 // et retourne le nombre de mots dans cette chaîne.
 // On suppose que les mots sont séparés uniquement par des espaces.
 // La fonction parcourt la chaîne caractère par caractère et compte les espaces
 // pour déterminer le nombre de mots.
 
 function count_words(string_of_words) {
+
  // Initialisation du compteur de mots à 1 car le nombre de mots est égal au nombre d'espaces + 1
 let wordCount = 1;
 
@@ -36,4 +37,24 @@ return wordCount;
 }
 
 
+// Partie 3: Cette fonction prend en paramètre une chaîne de caractères (string_of_words)
+// et un séparateur optionnel (letter, par défaut un espace).
+// Elle retourne le nombre de mots dans la chaîne, en utilisant le séparateur spécifié.
+// La fonction parcourt la chaîne caractère par caractère et compte les séparateurs
+// pour déterminer le nombre de mots.
+
+function count_words_by(string_of_words, letter = ' ') {
+    let wordCount = 0;
+    
+        // Parcours de la chaîne de caractères
+        for (let i = 0; i < string_of_words.length; i++) {
+            if (string_of_words.charAt(i) !== letter) {
+                if (i === 0 || string_of_words.charAt(i - 1) === letter) {
+                    wordCount++;
+                }
+            }
+        }   
+    return wordCount;    
+    
+}
     
